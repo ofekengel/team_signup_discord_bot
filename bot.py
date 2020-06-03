373a83ab70bedf65b03ccb0652a7d6359312b77b
ofek <ofek.engel@gmail.com>
2020-06-04 00:29:18 +0300

    added on off alerts

--- bot.py
+++ bot.py
@@ -15,19 +15,30 @@ class UnknownCommandException(Exception):
     pass
 
 
+OFFLINE_MESSAGE = """Dear Teams,
+
+The sign-up bot is offline right now. 
+The bot is online every day from 16:00 CET till 23:00 CET. Please try it again tomorrow!
+
+Thanks for your patience, 
+The technic team"""
+
+
 class Bot(discord.Client):
     LOGIN_MESSAGE = '{} online!'
-    # BOT_CHANNEL_ID = 711623834507411466
 
     def __init__(self, **options):
         super().__init__(**options)
         self.__command_parser_router = ParserRouter
         self.__command_handler_router = CommandHandlerRouter
         self.awaiting_response: List[Member] = []
+        self.BOT_CHANNEL_ID = 0
 
     async def on_ready(self):
+        self.BOT_CHANNEL_ID = discord.utils.get(discord.utils.get(self.guilds, name='pt').channels, name='bot').id
         print(self.LOGIN_MESSAGE.format(self.user.display_name))
-        # await self.get_channel(self.BOT_CHANNEL_ID).send('online!')
+        await self.__delete_last_bot_status_message()
+        await self.__send_bot_status_message('online!')
 
     async def on_message(self, message: discord.Message):
         if not message.author.bot and not message.type == discord.MessageType.pins_add:
@@ -47,7 +58,12 @@ class Bot(discord.Client):
                     except CommandParserException as e:
                         await message.channel.send(e.args[0])
             else:
-                await message.channel.send('dm not supported yet')
+                if message.author.id == 158297103276310528 and message.content == 'shutdown':
+                    await self.__delete_last_bot_status_message()
+                    await self.__send_bot_status_message(OFFLINE_MESSAGE)
+                    await self.logout()
+                else:
+                    await message.channel.send('dm not supported yet')
 
     def __parse_message(self, raw_message: str) -> ICommand:
         lines = raw_message.split('\n')
@@ -71,6 +87,17 @@ class Bot(discord.Client):
     def __trim_command(raw_message):
         command_end_index_in_message = raw_message.find(' ') + 1
         return raw_message[command_end_index_in_message:]
+
+    async def __delete_last_bot_status_message(self) -> None:
+        with open('message_to_delete.txt', 'r') as f:
+            message_to_delete_id = f.read()
+        await self.http.delete_message(self.BOT_CHANNEL_ID, message_to_delete_id)
+
+    async def __send_bot_status_message(self, message: str) -> None:
+        shutdown_message = await self.get_channel(self.BOT_CHANNEL_ID).send(
+            message)
+        with open('message_to_delete.txt', 'w') as f:
+            f.write(str(shutdown_message.id))
 
 
 if __name__ == '__main__':
